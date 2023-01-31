48c52ca4c09c328c66f056b9360f79d296101b86
Robert Bradford <15946+robertwbradford@users.noreply.github.com>
2023-01-30 17:19:28 -0700

    Read env variable

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        SOMETHING: {process.env.SOMETHING}
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
