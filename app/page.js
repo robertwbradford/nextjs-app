c188d5f4a9646b2ddeff0d012f4aea9cb4d4d023
Robert Bradford <15946+robertwbradford@users.noreply.github.com>
2025-07-29 17:08:03 -0600

    Update SOMETHING label

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        SOMETHING: {process.env.SOMETHING}
+        Here is SOMETHING: {process.env.SOMETHING}
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
